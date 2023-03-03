0666b4daff4b85a372a8aa846d77ea3480d14638
Othman Shawgan <oth1998@gmail.com>
2023-03-03 23:54:28 +0200

    add DikhrNotifier usage

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_vibrate/flutter_vibrate.dart';
+import 'package:provider/provider.dart';
 
+import '../services/dikhr_service.dart';
 import 'settings_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,19 +16,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter(DikhrNotifier dikhr) {
     setState(() {
       _counter++;
     });
-    if (_counter % 10 != 0) {
-      Vibrate.feedback(FeedbackType.medium);
+    if (_counter % dikhr.getDikhrTarget() != 0) {
+      //vibrate on tap
+      if (dikhr.getVibrateOnTap()) {
+        Vibrate.feedback(FeedbackType.medium);
+      }
     } else {
-      Vibrate.feedback(FeedbackType.heavy);
+      //vibrate on target reach
+      if (dikhr.getVibrateOnCountTarget()) {
+        Vibrate.feedback(FeedbackType.warning);
+      }
     }
   }
 
   void _resetCounter() async {
-    Vibrate.feedback(FeedbackType.warning);
+    Vibrate.feedback(FeedbackType.error);
     setState(() {
       _counter = 0;
     });
@@ -34,41 +42,45 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: 'Settings',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SettingsPage(
-                    title: 'Settings Page',
+    return Consumer<DikhrNotifier>(
+      builder: (context, dikhr, _) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: 'Settings',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SettingsPage(
+                      title: 'Settings Page',
+                    ),
                   ),
+                );
+              },
+            ),
+          ],
+        ),
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onLongPress: _resetCounter,
+          onTap: (() {
+            _incrementCounter(dikhr);
+          }),
+          child: Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.width * 0.6,
+              width: MediaQuery.of(context).size.width * 0.95,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  color: Colors.black54,
                 ),
-              );
-            },
-          ),
-        ],
-      ),
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onLongPress: _resetCounter,
-        onTap: _incrementCounter,
-        child: Center(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.width * 0.6,
-            width: MediaQuery.of(context).size.width * 0.95,
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                color: Colors.black54,
-              ),
-              child: Center(
-                child: Text('$_counter', style: Theme.of(context).textTheme.headline1),
+                child: Center(
+                  child: Text('$_counter', style: Theme.of(context).textTheme.headline1),
+                ),
               ),
             ),
           ),
